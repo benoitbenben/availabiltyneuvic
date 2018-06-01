09848ba3035dfcc41c941694915f44e7e3a97eb8
benoitbenben <benoit.mathonat@gmail.com>
2018-06-01 16:06:59 +0200

    configuration of routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,14 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # par défaut nous sommes dans le cas du renter :
+  resources :houses, only: [:index, :show] do
+    resources :bookings, only: :create
+  end
+
+  resources :bookings, only: [:index, :show]
+
+  # profile utilisateur
+  resource :profile, only: [:show, :edit, :update]
 end
