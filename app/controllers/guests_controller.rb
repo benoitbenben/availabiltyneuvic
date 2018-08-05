34d8976905e68ac531435fbd6a07feb74c8192fc
benoitbenben <benoit.mathonat@gmail.com>
2018-08-05 21:52:22 +0200

    controller guest & design list booking

---
+++ app/controllers/guests_controller.rb
@@ -0,0 +1,12 @@
+class GuestsController < ApplicationController
+ before_action :authenticate_user!, only: [:index, :show]
+
+  def index
+    @guests = guest.all
+  end
+
+  def show
+    @guest = Guest.find(params[:id])
+    @booking = Booking.new # On crée un objet vide pour simple_form
+  end
+end
