2a3cecf617d1112c26007cedac2f7b6378b5ef2d
benoitbenben <benoit.mathonat@gmail.com>
2019-10-02 10:10:55 +0800

    flatpickr with range for selection

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -1,18 +1,15 @@
 //app/javascript/plugins/flatpickr.js
 import flatpickr from "flatpickr"
-import "flatpickr/dist/flatpickr.min.css" // Note this is important!
+// import "flatpickr/dist/flatpickr.min.css" // Note this is important!
 import "flatpickr/dist/themes/airbnb.css" // A path to the theme CSS
+import rangePlugin from "flatpickr/dist/plugins/rangePlugin"
 
-flatpickr(".datepicker", {})
 
-
-// app/javascript/plugins/flatpickr.js
-// import flatpickr from "flatpickr"
-// import "flatpickr/dist/flatpickr.min.css"
-// import rangePlugin from "flatpickr/dist/plugins/rangePlugin"
-
-// flatpickr("datepicker", {
-//   altInput: true,
-//   enableTime: true
-//   plugins: [new rangePlugin({ input: "#end_date"})]
-// })
+flatpickr("#start_date", {
+  // mode: "range",
+  minDate: "today",
+  altInput: true,
+  altFormat: "F j, Y",
+  dateFormat: "Y-m-d",
+  plugins: [new rangePlugin({ input: "#end_date"})]
+})
