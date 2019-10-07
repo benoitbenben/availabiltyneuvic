fcd2415f2ff23f2e53ba52de94383b3122a23aa0
benoitbenben <benoit.mathonat@gmail.com>
2019-10-07 18:23:29 +0800

    fixing bug flatpickr select dates

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -1,6 +1,6 @@
 //app/javascript/plugins/flatpickr.js
 import flatpickr from "flatpickr"
-// import "flatpickr/dist/flatpickr.min.css" // Note this is important!
+import "flatpickr/dist/flatpickr.min.css" // Note this is important!
 import "flatpickr/dist/themes/airbnb.css" // A path to the theme CSS
 import rangePlugin from "flatpickr/dist/plugins/rangePlugin"
 
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,6 +60,9 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
+  # gem railties upgrade to 5.2.2.1
+  config.secret_key_base = SecureRandom.hex(64)
+
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
